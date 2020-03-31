6d96d6168061f6fa0aadf33a0a9ea356f57b7d8e
ornel77 <t.ornel@yahoo.fr>
2020-03-31 22:30:30 +0200

    responsive and nav for mobile ok

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import logo from '../images/logo.svg'
 
-const Navbar = props => {
-    return (
-        <div className="container">
-            <nav>
-                <div className="logo">
-                    <img src={logo} alt="logo" />
-                </div>
-                <button className="hamburger">
-                    <i className="fas fa-bars fa-2x"></i>
-                </button>
-                <div className="navbar">
-                    <ul className="navlist">
-                        <li><a href="#">Features</a></li>
-                        <li><a href="#">Pricing</a></li>
-                        <li><a href="#">Resources</a></li>
-                    </ul>
-                    <ul className="login">
-                        <li><a href="#">Login</a></li>
-                        <li><a href="#" className="btn">Sign Up</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </div >
-    )
+
+class Navbar extends Component {
+    handleClick() {
+        const hamburger = document.querySelector('.hamburger')
+        hamburger.addEventListener('click', () => {
+            document.body.classList.toggle('show-nav')
+            console.log('hello');
+        })
+    }
+
+    render() {
+
+
+        return (
+            <div className="container">
+                <nav>
+                    <div className="logo">
+                        <img src={logo} alt="logo" />
+                    </div>
+                    <button className="hamburger" onClick={this.handleClick} >
+                        <i className="fas fa-bars fa-2x"></i>
+                    </button>
+                    <div className="navbar">
+                        <ul className="navlist">
+                            <li><a href="#">Features</a></li>
+                            <li><a href="#">Pricing</a></li>
+                            <li><a href="#">Resources</a></li>
+                        </ul>
+                        <ul className="login">
+                            <li><a href="#">Login</a></li>
+                            <li><button href="#" className="btn">Sign Up</button></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div >
+        )
+    }
 }
 
 
